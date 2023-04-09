5e051c4cc6711cc4e490eeef072d42de713da634
omaryeso <67795148+omaryeso@users.noreply.github.com>
2023-04-08 21:46:13 -0500

    Configuration modal

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  public modalSwitch : any;
+
+  openModal()
+  {
+      this.modalSwitch = true;
+  }
 
 }
