be1a4a2f2ea80cfb5b12e69251be9456580cbfc9
omaryeso <67795148+omaryeso@users.noreply.github.com>
2023-04-14 14:56:36 -0500

    Changes in home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { HotelI } from 'src/app/models/hotel.interface';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-
+  hotels: HotelI[] = [];
+  constructor(private dataService: DataService) {}
+  ngOnInit(){
+    /* Get Hoteles */ 
+    const url = 'https://localhost:7066/api/hotels';
+    this.dataService.get<HotelI[]>(url).subscribe(res => {
+     this.hotels =  res.body;
+     console.log(this.hotels);
+    }),
+    err=> {
+      console.log('No se pudieron cargar las ubicaciones', err);
+    }
+  }
 }
